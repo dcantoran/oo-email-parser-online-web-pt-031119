46af2c13b0572dc64c5bfd52f7468ddeebbb2b46
David Cantoran <davidcantoran36@gmail.com>
2019-04-08 02:31:12 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,9 +5,13 @@
 
 
 class EmailParser 
+  attr_accessor :email 
   
+  def initialize(email)
+    @email = email
+  end 
   def parse 
-    
+    email.split(/[,?\s]/).select{|email| email != ""}.uniq 
   end 
   
 end 
